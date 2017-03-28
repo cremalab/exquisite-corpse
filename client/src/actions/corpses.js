d3ee5b3f891b320f4e1403d0315c1ed57fe4a0a4
Ross Brown <ross@cremalab.com>
2017-03-28 14:16:00 -0500

    sending cookies with fetch requests

--- client/src/actions/corpses.js
+++ client/src/actions/corpses.js
@@ -1,22 +1,24 @@
-import { CALL_API } from 'redux-api-middleware';
+import { CALL_API } from 'redux-api-middleware'
 
 export function loadCorpses() {
   return {
     [CALL_API]: {
       endpoint: '/corpses',
       method: 'GET',
-      types: ['REQUEST_CORPSES', 'SUCCESS_CORPSES', 'FAILURE']
-    }
+      types: ['REQUEST_CORPSES', 'SUCCESS_CORPSES', 'FAILURE'],
+      credentials: 'include',
+    },
   }
 }
 
 export function loadCorpse(id) {
   return {
     [CALL_API]: {
-      endpoint: `/corpses/${id}`, //58cc32a4bbf3b742adf1bbb8'
+      endpoint: `/corpses/${id}`, // 58cc32a4bbf3b742adf1bbb8'
       method: 'GET',
-      types: ['REQUEST_CORPSE', 'SUCCESS_CORPSE', 'FAILURE']
-    }
+      types: ['REQUEST_CORPSE', 'SUCCESS_CORPSE', 'FAILURE'],
+      credentials: 'include',
+    },
   }
 }
 
@@ -26,6 +28,7 @@ export function createCorpse() {
       endpoint: '/corpses',
       method: 'POST',
       types: ['REQUEST_CORPSE_CREATE', 'SUCCESS_CORPSE_CREATE', 'FAILURE'],
-    }
+      credentials: 'include',
+    },
   }
 }
--- client/src/actions/drawings.js
+++ client/src/actions/drawings.js
@@ -1,18 +1,19 @@
-import { push } from 'react-router-redux';
-import { CALL_API } from 'redux-api-middleware';
+import { push } from 'react-router-redux'
+import { CALL_API } from 'redux-api-middleware'
 
 export function loadDrawing(id) {
   return {
     [CALL_API]: {
       endpoint: `/drawings/${id}`,
       method: 'GET',
-      types: ['REQUEST', 'SUCCESS_CORPSE', 'FAILURE']
-    }
+      types: ['REQUEST', 'SUCCESS_CORPSE', 'FAILURE'],
+      credentials: 'include',
+    },
   }
 }
 
 export function setDrawing(payload) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch({ type: 'DRAWING_SET', payload })
   }
 }
