64a26af181eb8919c5dcfb789a5b6ccf9cb7049e
Rob LaFeve <rob@cremalab.com>
2017-04-04 16:23:18 -0500

    Update corpse response schema and add joi-react
    `joi-react` allows us to convert Joi schemas to React propTypes 👍

--- client/src/components/ItemCorpse/index.js
+++ client/src/components/ItemCorpse/index.js
@@ -1,7 +1,20 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 import Box from 'react-boxen'
+import propTypesCorpse from 'propTypes/Corpse'
+
+const css = {
+  container: `
+    padding: 20px;
+    borderRadius: 6px;
+    background-color: hsl(0, 0%, 98%);
+    cursor: pointer;
+    &:hover {
+      background-color: hsl(0, 0%, 95%)
+    }
+  `
+}
 
 class ItemCorpse extends PureComponent {
   render() {
@@ -11,24 +24,19 @@ class ItemCorpse extends PureComponent {
     })
     return (
       <Box
-        backgroundColor='hsl(0, 0%, 98%)'
-        borderRadius='6px'
-        padding='10px'
         onClick={() => dispatch(push(`/corpse/${corpse._id}`))}
-        css={`
-          cursor: pointer;
-          &:hover {
-            background-color: hsl(0, 0%, 95%)
-          }
-        `}>
-        <Box>
-          <p>Corpse with {openSections.length}/{corpse.sections.length} open spots</p>
-          <p>Created by {corpse.creator.name} on {corpse.createdAt}</p>
-          <cite>{corpse.status}</cite>
-        </Box>
+        css={css.container}>
+        <p>Corpse with {openSections.length}/{corpse.sections.length} open spots</p>
+        <p>Created by {corpse.creator.name} on {corpse.createdAt}</p>
+        <cite>{corpse.status}</cite>
       </Box>
     )
   }
+}
+
+ItemCorpse.propTypes = {
+  dispatch: PropTypes.func,
+  corpse: propTypesCorpse
 }
 
 export default connect()(ItemCorpse)
