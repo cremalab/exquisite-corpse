417e8a3e89c65f9b12f2037860b1b2fbee581d76
Deric Mendez <deric@cremalab.com>
2017-04-10 11:11:18 -0500

    remove old actions, clean up index

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from './Root'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-
-injectTapEventPlugin()
 
 const rootElement = document.getElementById('root')
 ReactDOM.render( <Root />, rootElement )
-
-if (module.hot) {
-  module.hot.accept('./Root', () => {
-    var NextApp = require('./Root').default
-    ReactDOM.render(<NextApp />, rootElement)
-  })
-}
