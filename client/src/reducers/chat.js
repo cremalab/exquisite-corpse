3a2951bfbdb93d6fc8c0b1466a709f5560fe6b58
Ross Brown <ross@cremalab.com>
2017-04-17 15:13:35 -0500

    removing now-redundant user status update messages

--- client/src/reducers/chat.js
+++ client/src/reducers/chat.js
@@ -1,6 +1,5 @@
 import {
   CHAT_MESSAGE_ADD,
-  USER_STATUS_CHANGE,
   LOBBY_EVENT,
 } from 'config/actionTypes'
 
@@ -8,10 +7,6 @@ const initialState = {
   messages: [],
 }
 
-const statuses = {
-  idle: 'in lobby',
-  drawing: 'drawing',
-}
 
 function chat(state = initialState, action) {
   switch (action.type) {
@@ -19,18 +14,6 @@ function chat(state = initialState, action) {
       return {
         ...state,
         messages: [...state.messages, action.payload],
-      }
-    case USER_STATUS_CHANGE:
-      var { name, status, timestamp } = action.payload
-      var message = {
-        content: `${name} is now ${statuses[status]}`,
-        name: 'system',
-        id: 0,
-        timestamp,
-      }
-      return {
-        ...state,
-        messages: [...state.messages, message]
       }
     case LOBBY_EVENT:
       return {
