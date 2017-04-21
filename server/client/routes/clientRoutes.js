a078576133613b95d7f4a41e1e7cb7362638aedc
Deric Mendez <deric@cremalab.com>
2017-04-21 16:40:32 -0500

    attempt to validate routes with specific headers... still working

--- server/client/routes/clientRoutes.js
+++ server/client/routes/clientRoutes.js
@@ -1,4 +1,5 @@
 const corpsesDB = require('../../corpses/db/corpsesDB')
+const Joi = require('joi')
 
 module.exports = [
   {
@@ -24,24 +25,29 @@ module.exports = [
     method: 'GET',
     path: '/corpse/{id}',
     config: {
-      auth: false
+      auth: false,
+      validate: {
+        headers: Joi.object({
+          'user-agent': Joi.string().regex(/facebookexternalhit/i).required()
+        }).options({ allowUnknown: true })
+      }
     },
     handler: function (request, reply) {
 
-      if (
-        request.headers['user-agent'].search(/bot/i) > -1 ||
-        request.headers['user-agent'].search(/facebookexternalhit/i) > -1
-      ) {
-        const { db } = request.mongo
-        corpsesDB.find(db, request.params.id).then((data) => {
-          reply.view('index.html', {
-            pageTitle: 'Corpse created by ' + data.creator.name,
-            pageImage: data.svgUrl,
-          })
+      //if (
+      //  request.headers['user-agent'].search(/bot/i) > -1 ||
+      //  request.headers['user-agent'].search(/facebookexternalhit/i) > -1
+      //) {
+      const { db } = request.mongo
+      corpsesDB.find(db, request.params.id).then((data) => {
+        reply.view('index.html', {
+          pageTitle: 'Corpse created by ' + data.creator.name,
+          pageImage: data.svgUrl,
         })
-      } else {
-        reply.close()
-      }
+      })
+      //} else {
+      //  //reply.continue()
+      //}
 
     }
   }
