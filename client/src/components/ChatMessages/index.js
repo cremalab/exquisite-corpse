ae68e26b5fe489a242a69bd3cdea8c988dc0c99e
Rob LaFeve <rob@cremalab.com>
2017-04-21 14:15:44 -0500

    Fix formatting of debounce function

--- client/src/components/ChatMessages/index.js
+++ client/src/components/ChatMessages/index.js
@@ -28,18 +28,18 @@ const ScrollPane = styled.div`
 `
 
 function debounce(func, wait, immediate) {
-    var timeout
-    return function() {
-        var context = this, args = arguments
-        var later = function() {
-            timeout = null
-            func.apply(context, args)
-        }
-        var callNow = immediate && !timeout
-        clearTimeout(timeout)
-        timeout = setTimeout(later, wait)
-        if (callNow) func.apply(context, args)
-            }
+  var timeout
+  return function() {
+    var context = this, args = arguments
+    var later = function() {
+      timeout = null
+      func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
 }
 
 
