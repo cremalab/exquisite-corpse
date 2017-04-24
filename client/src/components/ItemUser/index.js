274d99c121afee6f272ef6900926af8a31c96efc
Ross Brown <ross@cremalab.com>
2017-04-24 14:56:14 -0500

    adding typing and drawing status indicators

--- client/src/components/ItemUser/index.js
+++ client/src/components/ItemUser/index.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Box from 'react-boxen'
+import Icon from 'components/Icon'
+import TypingIndicator from './TypingIndicator'
+import DrawingIndicator from './DrawingIndicator'
 import spacing from 'config/spacing'
 import colors from 'config/colors'
 
+function renderStatusMarkup(status) {
+  switch (status) {
+    case 'idle':
+      return <Icon glyph='user' />
+    case 'drawing':
+      return <DrawingIndicator />
+    case 'typing':
+      return <TypingIndicator />
+    default:
+      return 'user'
+  }
+}
+
 const ItemUser = ({ status, name, provider }) =>
-  <Box {...boxProps} >
-    <div>{ status }</div>
+  <Box {...boxProps} childSpacing={spacing[6]} >
+    <Box
+      childSpacing={spacing[4]}
+      childDirection='row'>
+      {renderStatusMarkup(status)}
+    </Box>
     <div>{ name }</div>
     <div>{ provider }</div>
   </Box>
