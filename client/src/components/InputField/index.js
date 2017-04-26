b639380a50b40c818651950e6762d61444bccf23
Deric Mendez <deric@cremalab.com>
2017-04-26 10:20:52 -0500

    while at it, centralize Input styles and filed to InputField component since it plugs in well with redux forms as a component

--- client/src/components/InputField/index.js
+++ client/src/components/InputField/index.js
@@ -1,5 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import spacing from 'config/spacing'
+import colors from 'config/colors'
+
+const Input = styled.input`
+  width: 100%;
+  border: none;
+  background: white;
+  font-size: ${spacing[5]};
+  padding: ${spacing[4]};
+  border-radius: ${spacing[3]};
+  box-shadow: 0 0 ${spacing[3]} hsl(0, 0%, 80%);
+  border: 1px solid white;
+  &:focus {
+    outline: 0;
+    border-color: ${props => colors[`${props.skin}-tint-3`]};
+  }
+`
 
 const InputField = ({
   input,
@@ -12,11 +30,15 @@ const InputField = ({
   <div>
     <label>{label}</label>
     <div>
-      <input {...input} {...rest} type={type} placeholder={placeholder || label}/>
+      <Input {...input} {...rest} type={type} placeholder={placeholder || label}/>
       {touched && error && <span>{error}</span>}
     </div>
   </div>
 )
+
+InputField.defaultProps = {
+  skin: 'secondary'
+}
 
 InputField.propTypes = {
   input: PropTypes.object,
