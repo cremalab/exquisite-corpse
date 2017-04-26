d511a2491c116db3b8f0d11ba62d81bef61332e2
Ross Brown <ross@cremalab.com>
2017-04-26 15:33:04 -0500

    fixing mongo uri for tests in a bad way

--- db/testHelper.js
+++ db/testHelper.js
@@ -1,5 +1,11 @@
 const MongoClient = require('mongodb').MongoClient
-const mongoURI = process.env.MONGO_TEST_URI || 'mongodb://localhost:27017/exquisite-test'
+let mongoURI
+if (process.env.NODE_ENV === 'test') {
+  if (process.env.HEROKU_TEST_RUN_ID) { mongoURI = process.env.MONGODB_URI }
+  else { mongoURI = process.env.MONGODB_TEST_URI || 'mongodb://localhost:27017/exquisite-test'}
+} else {
+  mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/exquisite-corpse'
+}
 
 module.exports = {
   connectDB() {
