6c9c783b294ab046dab42ce59fd4f9838e1fd327
Ross Brown <ross@cremalab.com>
2017-04-26 14:29:54 -0500

    logging env variables

--- server/utils/testHelper.js
+++ server/utils/testHelper.js
@@ -7,6 +7,9 @@ module.exports = {
     provider: 'Slack',
   },
   testServer() {
+    console.log('mongo test uri', process.env.MONGODB_TEST_URI);
+    console.log('mongo uri', process.env.MONGODB_URI);
+    console.log('env dump', process.env);
     return compose()
   },
 }
