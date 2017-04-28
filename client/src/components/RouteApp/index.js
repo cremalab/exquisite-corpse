0ead201763a2248167ded7b4c3bae7c96f215cd8
Ross Brown <ross@cremalab.com>
2017-04-28 15:44:14 -0500

    adding chat sidebar to create view cuz why not?

--- client/src/components/RouteApp/index.js
+++ client/src/components/RouteApp/index.js
@@ -64,6 +64,7 @@ class RouteApp extends Component {
         sidebar={
           <RoutesContainer data-grow>
             <Route exact path="/" component={RouteMessagesGlobal} />
+            <Route exact path="/create" component={RouteMessagesGlobal} />
             <Route exact path="/drawing/:drawingId" component={RouteDrawingSidebar} />
             <Route exact path="/corpse/:corpseId" component={RouteMessagesGlobal} />
           </RoutesContainer>
